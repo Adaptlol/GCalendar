2c92d7ccfc319a92bb072c39fa51f2a318e76199
laoneo <allon.moritz@digital-peak.com>
2013-04-19 09:16:12 +0000

    Tagged as version 3.0.1

---
+++ GCalendar/com_gcalendar/site/views/gcalendar/tmpl/gcalendar.js
@@ -0,0 +1,32 @@
+gcjQuery(document).ready(function(){
+	gcjQuery('#gc_gcalendar_view_toggle_status').bind('click', function(e) {
+		gcjQuery('#gc_gcalendar_view_list').slideToggle('slow', function(){
+			var oldImage = gcjQuery('#gc_gcalendar_view_toggle_status').attr('src');
+			var gcalImage = oldImage;
+			var path = oldImage.substring(0, oldImage.lastIndexOf('/'));
+			
+			if (gcjQuery('#gc_gcalendar_view_list').is(":hidden"))
+				gcalImage = path + '/down.png';
+			else
+				gcalImage = path + '/up.png';
+			
+			gcjQuery('#gc_gcalendar_view_toggle_status').attr('src', gcalImage);
+		});
+	});
+});
+
+function updateGCalendarFrame(calendar) {
+	if (calendar.checked) {
+		gcjQuery('#gcalendar_component').fullCalendar('addEventSource', calendar.value);
+	} else {
+		gcjQuery('#gcalendar_component').fullCalendar('removeEventSource', calendar.value);
+	}
+}
+
+function printView() {
+	var loc=document.location.href.replace(/\?/,"\?layout=print&format=raw\&");
+	if (loc==document.location.href)
+		loc=document.location.href.replace(/#/,"\?layout=print&format=raw#");
+	var printWindow = window.open(loc);
+	printWindow.focus();
+}
